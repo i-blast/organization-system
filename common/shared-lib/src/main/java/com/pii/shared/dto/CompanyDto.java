e2988a072b408264df5da9fa155fcdcef20eae88
ilYa <EnableWebSocket@yandex.ru>
2025-04-21 18:12:11 +0500

    refactor: remove unnecessary comments

--- common/shared-lib/src/main/java/com/pii/shared/dto/CompanyDto.java
+++ common/shared-lib/src/main/java/com/pii/shared/dto/CompanyDto.java
@@ -7,32 +7,20 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-/**
- * Company data transfer object.
- *
- * @author ilYa
- */
 @Builder
 @Data
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 public final class CompanyDto {
 
-    /**
-     * Company id.
-     */
     private Long id;
-    /**
-     * Company name.
-     */
+
     private String name;
     /**
      * Company budget in USD cents.
      */
     private Long budget;
-    /**
-     * List of company short view employee dtos.
-     */
+
     private List<UserShortDto> employees;
 
 }
--- common/shared-lib/src/main/java/com/pii/shared/dto/UserDto.java
+++ common/shared-lib/src/main/java/com/pii/shared/dto/UserDto.java
@@ -5,40 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * User data transfer object.
- *
- * @author ilYa
- */
 @Builder
 @Data
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 public final class UserDto {
 
-    /**
-     * User id.
-     */
     private Long id;
 
-    /**
-     * User first name.
-     */
     private String firstName;
 
-    /**
-     * User last name.
-     */
     private String lastName;
 
-    /**
-     * User phone number.
-     */
     private String phoneNumber;
 
-    /**
-     * Short view company dto.
-     */
     private CompanyShortDto company;
 
 }
